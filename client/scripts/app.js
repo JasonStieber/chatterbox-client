c3d6903db2256752b3043a86daf589673289ce4c
Hack Reactor Student <student@hackreactor.com>
2017-07-14 16:29:39 -0700

    Most tests passing, working on friendster v2

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,57 @@
 // YOUR CODE HERE:
+
+class ChatterClient {
+  constructor() {
+    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+    this.$chatFeed = $('#chats');
+    this.user = {users: []};
+  }
+  init() {
+    
+  }
+  _error(data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  } 
+  send(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: this.error
+    });
+  }
+  fetch() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        // take care of bizzzness 
+      },
+      error: this.error
+    });
+  }
+  clearMessages() {
+    this.$chatFeed.empty();
+  }
+  renderMessage(message) {
+    var $thing = $('<p></p>');
+    $thing.html(JSON.stringify(message));
+    this.$chatFeed.append($thing);
+  }
+  renderRoom(roomName) {
+    var $optionThing = $(`<option value="${roomName}">${roomName}</option>`);
+    $('#roomSelect').append($optionThing);
+  }
+}
+
+$(function() {
+  window.app = new ChatterClient();
+});
