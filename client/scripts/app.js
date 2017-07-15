048ab771c10214861458926cb5c92a83103ed460
Hack Reactor Student <student@hackreactor.com>
2017-07-14 20:45:46 -0700

    Fetch is working, attacks are commencing

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,9 +5,11 @@ class ChatterClient {
     this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
     this.$chatFeed = $('#chats');
     this.user = {users: []};
+
+    this.init();
   }
   init() {
-    
+    this.fetch();
   }
   _error(data) {
     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -21,19 +23,31 @@ class ChatterClient {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log(data);
       },
       error: this.error
     });
+
+    this.renderMessage(message);
   }
   fetch() {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: this.server,
+      url: this.server + '?order=-updatedAt',
       type: 'GET',
       contentType: 'application/json',
-      success: function (data) {
-        // take care of bizzzness 
+      success: (data) => {
+        // take care of bizzzness
+        var results = data.results.reverse().filter(function(item) {
+          return (item.hasOwnProperty('text') &&
+            item.hasOwnProperty('roomname') &&
+            item.hasOwnProperty('username'));
+        });
+        
+        console.log(results);
+        results.forEach((item) => {
+          this.renderMessage(item);
+        });
       },
       error: this.error
     });
@@ -41,14 +55,30 @@ class ChatterClient {
   clearMessages() {
     this.$chatFeed.empty();
   }
-  renderMessage(message) {
-    var $thing = $('<p></p>');
-    $thing.html(JSON.stringify(message));
-    this.$chatFeed.append($thing);
+  renderMessage({username, text, roomname, updatedAt = 'The Beginning'}) {
+    var $thing = $('<div></div>');
+
+    var $userName = $(`<a href="#" class=".username" data-username="${username}">${username}:</a>`);
+    $userName.on('click', (event) => this.handleUsernameClick(event.target));
+    $thing.html($userName);
+    
+    var $text = $('<div class="userMessage"></div>');
+    $text.append(document.createTextNode(text));
+    // add time later
+    $thing.append($text);
+
+    this.$chatFeed.prepend($thing);
   }
   renderRoom(roomName) {
     var $optionThing = $(`<option value="${roomName}">${roomName}</option>`);
     $('#roomSelect').append($optionThing);
+  }
+
+  handleUsernameClick(userNode) {
+    console.log($(userNode).data('username'));
+  }
+  handleSubmit(){
+
   }
 }
 
